877e93392854105b41b1b8116ac87cf764163c5d
Kk3tillen <k.kezia@academico.ifrn.edu.br>
2024-08-08 09:16:31 -0300

    add prova presencial

--- prova-03/contPalvras.py
+++ prova-03/contPalvras.py
@@ -1,9 +1,12 @@
 frase = input()
-palavras = frase.split()
 palavras_impressas = set()
 
-for palavra in palavras:
-    if palavra not in palavras_impressas:
-        qnt_palavras = palavras.count(palavra)
-        print(f"{qnt_palavras}: {palavra}")
-        palavras_impressas.add(palavra)
+def encontrarPalavra(frase, palavras_impressas):
+    palavras = frase.split()
+    for palavra in palavras:
+        if palavra not in palavras_impressas:
+            qnt_palavras = palavras.count(palavra)
+            print(f"{qnt_palavras}: {palavra}")
+            palavras_impressas.add(palavra)
+
+encontrarPalavra(frase, palavras_impressas)
